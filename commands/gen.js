7076d19e93dcc45e434bff664de0e00f2bbfd9a8
Wyatt Schulte <schultewya@gmail.com>
2022-11-17 22:15:28 -0500

    added support for generating components in directories

--- commands/gen.js
+++ commands/gen.js
@@ -1,19 +1,24 @@
+import chalk from 'chalk'
 import { exec } from 'child_process'
 
 const templateReactComponent = (name) => {
+    const cleanedComponentName = name.match(/([^\/]*)$/)[0]
+
     return `
     import React from "react";
 
-    const ${name} = () => {
-        return <div>${name}</div>
+    const ${cleanedComponentName} = () => {
+        return <div>${cleanedComponentName}</div>
     }
 
-    export default ${name}
+    export default ${cleanedComponentName}
     `
 }
 
 const gen = (name) => {
     exec(`echo '${templateReactComponent(name)}' >${name}.tsx`)
+
+    console.log(chalk.green.bold(`Generated ${name}.tsx`))
 }
 
 export default gen;
